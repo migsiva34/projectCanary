c1706afd417bf70bebff1297bd9f75db6b60fc42
migsiva34 <89012074+migsiva34@users.noreply.github.com>
2021-12-21 13:19:20 +0000

    fix mongoParse

--- app.js
+++ app.js
@@ -27,9 +27,7 @@ const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify:false
+    useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
